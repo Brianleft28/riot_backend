7d6ded843d961c5c00c93ab521daf4e0fa74a4a4
Brian <41770471+Brianleft28@users.noreply.github.com>
2024-07-23 18:46:26 -0300

    update: instalar mongo

---
+++ src/routes/index.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { readdirSync } from "fs";
+
+const PATH_ROUTES = `${__dirname}/`;
+const router = Router();
+
+
+const cleanFileName = (fileName: string) => {
+    const file = fileName.split('.').shift();
+    return file;
+}
+
+readdirSync(PATH_ROUTES).filter((fileName) => {
+    const cleanName = cleanFileName(fileName);
+    if (cleanName !== 'index') {
+        import(`./${cleanName}`).then((moduleRoute)=> {
+            console.log(`Route /${cleanName} loaded`);
+            router.use(`/${cleanName}`, moduleRoute.default);
+        })
+    }
+})
+
+
+export default router;
