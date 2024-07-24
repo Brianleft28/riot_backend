73aa354c9131cca908392ccbdff938b3ab5710fc
Brian <41770471+Brianleft28@users.noreply.github.com>
2024-07-23 21:30:07 -0300

    update

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import { getAccountByGameNameTagLine, getSummonerByPuuid } from "../controllers/accountController";
 
 const router = Router();
